371e826fdfaba87a76f06aed17d6100b5897802d
Jemadean Dobrosielski <jemadean@gmail.com>
2018-08-27 11:59:56 -0700

    added contact and address book components

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,3 +3,16 @@ import React from 'react';
 import './contact.css';
 
 // The Contact component goes here.  It should be the default export.
+export default function Contact(props) {
+  return (
+      <section id={`contact-${props.index}`} className="contact">
+        <img src={props.photo} className="contact-photo" alt={`image for ${props.name}`} />
+        <h2 className="contact-name">{props.name}</h2>
+        <address className="contact-address">{props.address}</address>
+      </section>
+  );
+}
+
+Contact.defaultProps = {
+  text: ''
+};
